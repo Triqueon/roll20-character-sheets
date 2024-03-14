97259b012d5896c79ab7e52deb3ed3114b6674ea
Pascal Stein <pascal@hakigo.de>
2024-03-14 23:22:37 +0100

    Fix migration

--- Das_Schwarze_Auge_4-1/dev/js/migration.js
+++ Das_Schwarze_Auge_4-1/dev/js/migration.js
@@ -829,33 +829,34 @@ function migrateTo20230618(migrationChain) {
 		});
 	});
 
-	/*
-	Migration steps: rename attribute subtag1 to z_repraesentation and map its value*/
-	function migrateTo20240314(migrationChain) {
-		var caller = "migrateTo20240314";
-		debugLog(caller, "Invoked.");
-		let valueMap = {
-			"---": "",
-			"gildenmagisch": "Mag",
-			"elfisch": "Elf",
-			"druidisch": "Dru",
-			"satuarisch": "Sat",
-			"geodisch": "Geo",
-			"schelmisch": "Sch",
-			"scharlatanisch": "Srl",
-			"borbaradianisch": "Bor",
-			"kristallomantisch": "Ach"
-		}
-		safeGetAttrs(["subtag1"], function (v) {
-			let attrsToChange = {
-				"z_erstrepraesentation": valueMap[v[subtag1]]
-			}
-			safeSetAttrs(attrsToChange, {}, function () {
-				callNextMigration(migrationChain);
-			});
-		});
-		
-	}	
-
 }
+
+/*
+	Migration steps: rename attribute subtag1 to z_repraesentation and map its value
+*/
+function migrateTo20240314(migrationChain) {
+	var caller = "migrateTo20240314";
+	debugLog(caller, "Invoked.");
+	let valueMap = {
+		"---": "",
+		"gildenmagisch": "Mag",
+		"elfisch": "Elf",
+		"druidisch": "Dru",
+		"satuarisch": "Sat",
+		"geodisch": "Geo",
+		"schelmisch": "Sch",
+		"scharlatanisch": "Srl",
+		"borbaradianisch": "Bor",
+		"kristallomantisch": "Ach"
+	}
+	safeGetAttrs(["subtag1"], function (v) {
+		let attrsToChange = {
+			"z_erstrepraesentation": valueMap[v[subtag1]]
+		}
+		safeSetAttrs(attrsToChange, {}, function () {
+			callNextMigration(migrationChain);
+		});
+	});
+	
+}	
 /* migration end */
